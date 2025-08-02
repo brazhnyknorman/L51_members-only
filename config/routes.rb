4f5a583f62d4d2614c2d1f9addc6d480a00047e4
brazhnyknorman <brazhnyknorman@gmail.com>
2025-08-02 02:52:12 -0400

    Implement users#show route, view and controller (no authorization)

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
         sessions: "users/sessions", registrations: "users/registrations"
       }
 
-  resources :users, only: [ :index ]
+  resources :users, only: [ :index, :show ]
 
   root "users#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
